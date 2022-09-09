4c34999d3e37c580af9a12647099e7ba6b009932
NwekeMaxwell <nwekemaxwell37@gmail.com>
2022-09-08 17:28:20 -0700

    a C program that prints some code followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include<unistd.h>
 /**
- * main - prints the standard error sandwiched around the standard outpu/t
- * Return: 1
- **/
-int mai(void)
+ * main-program entry point.
+ * Return: 1 on success
+ */
+int main(void)
 {
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-	return(1);
+write(2,"and that piece of art is useful\"-Dora Korpar,2015-10-19\n",59);
+return(1);
 }
-
