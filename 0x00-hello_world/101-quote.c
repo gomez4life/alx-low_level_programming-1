17bfe1b0d3f22a0a1a3e13d37d0586200c8c32d8
NwekeMaxwell <nwekemaxwell37@gmail.com>
2022-09-08 18:04:37 -0700

    a C program that prints some code followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main-program entry point.
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ *   followed by a new line, to the standard error.
  * Return: 1 on success
  */
 int main(void)
 {
-write(2,"and that piece of art is useful\"-Dora Korpar,2015-10-19\n",59);
-return(1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
